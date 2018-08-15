d904441952d0760ab3b82887b46f191a49b136e6
Luke Secomb <luke.secomb@live.com>
2018-08-16 01:31:10 +1000

    fixed invalid lat / lon values

--- src/config/user-data.js
+++ src/config/user-data.js
@@ -1,7 +1,7 @@
 export const userData = {
-    "userName": "Steve",
-    "location": {
-        "lat": "-35.2384096",
-        "lon": "149.083832"
+    userName: "Steve",
+    location: {
+        lat: "35.2384",
+        lon: "149.0838"
     }
 }
