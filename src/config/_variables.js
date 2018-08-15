d9c961f1ac9827142383b0eb773f443dd844ad8c
Luke Secomb <luke.secomb@live.com>
2018-08-16 01:30:21 +1000

    adjusted variable syntax

---
+++ src/config/_variables.js
@@ -0,0 +1,13 @@
+export const fonts = {
+    sans: `'Montserrat', sans-serif`,
+    serif: '"Times New Roman", serif'
+}
+
+export const colors = {
+    gold: '#DDB462',
+    white: '#f1f1f1',
+    lightGrey: '#CBCBCB',
+    darkGrey: '#323232',
+    gradientGreyDark: '#0E0E0E',
+    gradientGreyLight: '#181818'
+}
