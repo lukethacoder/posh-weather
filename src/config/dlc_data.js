a0b628f056d4196c60404755d8a24bdff447e901
Luke Secomb <luke.secomb@live.com>
2018-08-30 00:48:30 +1000

    added dlc_data file

---
+++ src/config/dlc_data.js
@@ -0,0 +1,17 @@
+export const dlc_data = {
+  "Extended Current": {
+    "cost": "75"
+  },
+  "Daily Forecast": {
+    "cost": "100"
+  },
+  "Weekly Forecast": {
+    "cost": "250"
+  },
+  "Extensive Weather": {
+    "cost": "500"
+  },
+  "Classy Audio": {
+    "cost": "750"
+  },
+}
